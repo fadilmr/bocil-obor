2ffeacbfd705e57e3bb484cdfd7269d7e75b647a
Muhammad Fadil Maulana Akbar <m.muhammadfadil12@gmail.com>
2022-03-16 19:07:10 +0700

    add color change when enemy near light

--- bocil obor/Assets/Scripts/enemyRun.cs
+++ bocil obor/Assets/Scripts/enemyRun.cs
@@ -9,6 +9,8 @@ public class enemyRun : MonoBehaviour
     public GameObject obor;
     public AIPath aipath;
     private float dist;
+    public Light2D lt;
+    private Color changecolor;
 
     void Start()
     {
@@ -20,13 +22,18 @@ public class enemyRun : MonoBehaviour
     void Update()
     {
         dist = (obor.transform.position.x - transform.position.x);
-        if (dist <= 5 && dist >= -5)
+        if (dist <= 6 && dist >= -6)
         {
             aipath.maxSpeed = 0.5f;
+            changecolor = new Color32(224, 129, 117, 255);
+            lt.color = changecolor;
+
         }
         else
         {
             aipath.maxSpeed = 0;
+            changecolor = new Color32(243, 217, 177, 255);
+            lt.color = changecolor;
         }
     }
 }
