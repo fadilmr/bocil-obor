7696ea301bfa09b81f1550a90fa71f524b8456fd
Khoirul <khoirulfahmi44@gmail.com>
2022-03-16 14:14:44 +0700

    add function on Following Camera Light

--- bocil obor/Assets/Scripts/FollowingCameraLight.cs
+++ bocil obor/Assets/Scripts/FollowingCameraLight.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class FollowingCameraLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //buat var untuk menentukan kecepatan kursor saat bergerak
+    public float panSpeed = 0.1f;
 
-    // Update is called once per frame
-    void Update()
+    //buat var untuk menentukan tebal bordernya
+    public float panBorder = 1;
+
+    private void Update()
     {
+        //buat var yang menampung transform.position
+        Vector3 pos = transform.position;
+
+        //kalau input mouse secara horizontal ke atas 
+        if (Input.mousePosition.y >= Screen.height - panBorder)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+
+        //kalau input mouse secara horizontal ke bawah 
+        if (Input.mousePosition.y <= panBorder)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        } 
         
+        //kalau input mouse secara vertikal ke kanan 
+        if (Input.mousePosition.x >= Screen.width - panBorder)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        //kalau input mouse secara vertikal ke kiri 
+        if (Input.mousePosition.x <= panBorder)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        //kita return dengan mengasign value pos
+        transform.position = pos;
     }
 }
