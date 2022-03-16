9201a70c213e66541fc5b403f24ef78b73d536c4
Muhammad Fadil Maulana Akbar <m.muhammadfadil12@gmail.com>
2022-03-16 07:38:00 +0700

    first commit

---
+++ bocil obor/Assets/Scripts/StopMov.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class StopMov : MonoBehaviour
+{
+    public GameObject obor;
+    public AIPath aipath;
+    private float dist;
+
+    private void Start()
+    {
+        aipath.maxSpeed = 0;
+    }
+
+
+    private void Update()
+    {
+        dist = (obor.transform.position.x - transform.position.x);
+        if (dist <= 2 && dist >= -2)
+        {
+            aipath.maxSpeed = 0.7f;
+        } else
+        {
+            aipath.maxSpeed = 0;
+        }
+
+
+    }
+}
---
+++ bocil obor/Assets/Scripts/followMouse.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followMouse : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
+        transform.position = mousePosition;
+    }
+}
