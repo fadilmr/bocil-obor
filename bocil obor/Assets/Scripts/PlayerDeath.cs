b9a5e0b4341f5f6725fe19d8aa3337332d980921
Khoirul <khoirulfahmi44@gmail.com>
2022-03-17 12:16:32 +0700

    add function on Player Death
    dengan cara mengcompare jarak antar 2 objek

--- bocil obor/Assets/Scripts/PlayerDeath.cs
+++ bocil obor/Assets/Scripts/PlayerDeath.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    public GameObject compareObject;
+    void Update () {
+        if(Vector3.Distance(compareObject.transform.position, transform.position) < 0.5){
+            Debug.Log("Mati");
+        }
     }
 }
